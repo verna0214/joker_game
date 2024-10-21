895afef7d0ee1904d77ce4ac0bb60785b54d5259
verna0214 <yuhsichen0214@gmail.com>
2024-10-21 11:38:22 +0800

    feat: init 52 cards

--- script.py
+++ script.py
@@ -1,3 +1,13 @@
+def initialize_deck():
+  # Initialize cards
+  suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  values = [ i for i in range(1, 14) ] # 1 = A, 2~10, 11~13 = J, Q, K
+  deck = [(suit, value) for suit in suits for value in values]
+  # Add 1 joker
+  deck.extend([("Joker", "Black")])
+
+  return deck
+
 def main():
   # Initialize players
   num_players = int(input("Please enter the number of players: "))
@@ -8,6 +18,8 @@ def main():
   print("OK. Now please enter player name in sequence.")
   name_players = [str(input(f"{i}: ")) for i in range(1, num_players + 1)]
   print(name_players)
+
+  deck = initialize_deck()
 
 
 if __name__ == "__main__":
